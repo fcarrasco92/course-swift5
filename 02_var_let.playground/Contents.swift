891eca685791bf6ca42205ac57b7ac15a170a9d6
= <=>
2020-05-10 23:28:10 -0400

    learn about  variables and constant

---
+++ 02_var_let.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+// you can't change the value
+let maximumNumberOfLoginAttempts = 3
+
+// you can change the value
+var currentLoginAttempt = 0
+
+// for declare variable in the same line
+var x = 0.0, y = 0.0, z = 0.0
+
+
