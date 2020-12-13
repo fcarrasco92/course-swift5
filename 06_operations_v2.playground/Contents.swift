53518e3d607d7938a0153a35418721289d4ee2c4
Felipe Carrasco <felipe@bakers.dev>
2020-12-13 19:15:15 -0300

    learn operation comparation

--- 06_operations_v2.playground/Contents.swift
+++ 06_operations_v2.playground/Contents.swift
@@ -62,3 +62,51 @@ numberTest %= 2
 print("result % 2 to variable numberTest = \(numberTest)")
 
 
+// comparaciones
+// == igual que
+1 == 1 // TRUE
+1 == 2 // FALSE
+// != DIFERENTE QUE
+1 != 2 // TRUE
+
+// MAYOR QUE
+2 > 1 // TRUE
+1 > 2 // FALSE
+
+// MENOR QUE
+1 < 2 // TRUE
+2 < 1 // FALSE
+
+// MAYOR IGUAL QUE
+2 >= 1 // TRUE
+1 >= 1 // TRUE
+
+// MENOR IGUAL QUE
+2 <= 1 // FALSE
+1 <= 2 // TRUE
+
+let name = "Juan Gabriel"
+
+if name == "Juan Gabriel" {
+    print("Bienvenido \(name), eres invitado a la fiesta ")
+}else{
+    print ("Cuidado, ha aparecido un \(name) salvaje")
+}
+
+// la validación de tuplas comienza de izquierda a derecha.
+// luego por ejemplo primero el numero, en el caso de corresponder toma el segundo valor de la
+// tupla
+(1, "Juan Gabriel") < (2, "Ricardo Celis") // true
+(3, "Juan Gabriel") < (3, "Ricardo Celis") // true -> porque compara 3 , luego valida los nombres y J(Juan Gabriel) es menor que R(Ricardo Celis)
+(3, "Ricardo Celis") < (3, "Juan Gabriel") // false -> porque compara 3, luego valida los nombres y R no es menor J, por ende es false
+(3, "Ricardo") < (3, "Juan Gabriel") // false
+(4, "perro") == (4, "perro")
+
+// Los booleanos no se pueden validar con Mayor o menor
+//("perro", false) < ("perro", true)
+ 
+// Reto: crear una tupla con la edad de  José Miguel vs Felipe Carrasco
+(28, "Felipe Carrasco") < (31, "Juan Gabriel") // true --> por Felipe es menor que Juan
+(28, "Felipe Carrasco") > (31, "Juan Gabriel") // false --> porque Juan  es mayor que Felipe
+(28, "Felipe Carrasco") == (31, "Juan Gabriel") // false --> porque la edad ambos es diferente
+(28, "Felipe Carrasco") != (31, "Juan Gabriel") // true --> porque la edad de ambos es diferente
