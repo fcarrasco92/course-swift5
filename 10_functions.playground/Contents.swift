cc1fcc68ba1201a1a8bb6238319795878bbf0666
Felipe Carrasco <felipe@bakers.dev>
2021-04-12 22:40:30 -0400

    feature: learn how to work function with parametera

--- 10_functions.playground/Contents.swift
+++ 10_functions.playground/Contents.swift
@@ -10,3 +10,46 @@ func greeting(person: String) -> String {
 greeting(person: "Felipe Carrasco")
 greeting(person: "Isabel")
 greeting(person: "Luis")
+
+// Parámetros de entrada3/16
+
+func sayHelloWorld() -> String{
+    return "hello world"
+}
+
+sayHelloWorld()
+sayHelloWorld()
+sayHelloWorld()
+
+// con parametros
+
+func greeting(person: String, isMale: Bool) -> String {
+    if isMale{
+        return "Bienvenido Caballero \(person)"
+    }else{
+        return "Bienvenida Señorita \(person)"
+    }
+}
+
+greeting(person: "Felipe", isMale: true)
+greeting(person: "Isabel", isMale: false)
+
+// Challenge add new parameter age in the function and add some validation with this
+
+
+func greetingPerson(person: String, isMale: Bool, age: UInt8) -> String {
+
+    switch age {
+    case 0..<18:
+        return "Hola Bienvenid\(isMale ? "o" : "a") \(isMale ? "niño" : "niña") \(person)"
+    case 18..<30:
+        return "Hola Bienvenid\(isMale ? "o" : "a") joven \(person)"
+    case 30...:
+        return "Hola Bienvenid\(isMale ? "o" : "a") sr\(isMale ? ".":"a.") \(person)"
+    default:
+        return "Edad no puede ser menor a 0"
+    }
+}
+
+print(greetingPerson(person: "Felipe", isMale: true, age: 28))
+print(greetingPerson(person: "Enrique", isMale: true, age: 57))
