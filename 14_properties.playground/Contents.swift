f6f279d0a3536c751b055fac74447f65fbbf7425
Felipe Carrasco <felipe@bakers.dev>
2021-05-11 07:23:40 -0400

    feature: lazy loading function

--- 14_properties.playground/Contents.swift
+++ 14_properties.playground/Contents.swift
@@ -18,3 +18,22 @@ let rangeOfFourItems = FixedLengthRange(firstValue: 0, length: 4)
 // rangeOfFourItems.length = 5 // Cannot assign to property: 'rangeOfFourItems' is a 'let' constant
 
 
+// Lazy Stored Properties
+
+
+class DataImporter {
+    var fileName = "data.txt"
+}
+
+class DataManager{
+    lazy var importer = DataImporter()
+    var data = [String]()
+}
+
+let manager = DataManager()
+
+manager.data.append("Algo de datos")
+manager.data.append("mas datos")
+// hasta esta linea, el importer no ha sido creado...
+manager.importer.fileName
+// aquí la variable importer ha sido creado
